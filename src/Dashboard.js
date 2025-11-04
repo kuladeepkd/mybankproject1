970c19612dbf68efedc6477b77ca6f2ad8added7
@kuladeepkd <kukadeep17@gmail.com>
2025-11-04 12:25:02 +0530

    2nd change

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 const Dashboard = () => {
   const {id}= useParams();
     return (
-    <div>Dashbord content displaying:{id}</div>
+     <div><h1>Dashbord content displaying:{id}</h1></div>
   )
 }
 
